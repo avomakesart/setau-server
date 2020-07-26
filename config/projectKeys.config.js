65efe57aa35bccb85cc4a82ba4580754d4f0db8c
Alvaro Castillo <alvaro.castillo777@gmail.com>
2020-07-25 19:41:05 -0500

    Adding prod keys

--- config/projectKeys.config.js
+++ config/projectKeys.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   database: {
     connectionLimit: 10,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'b89c22dbd1a04f',
+    password: 'cfd8671d',
+    database: 'heroku_38e43dd30660a24',
   },
 }
