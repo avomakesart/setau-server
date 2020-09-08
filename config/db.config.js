202c8d7f04c2fc1d82ab6f1c59db3e1eea92f013
Alvaro Castillo <alvaro.castillo777@gmail.com>
2020-09-08 02:54:57 -0500

    Updating db values postgres prod

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  HOST: process.env.DB_HOST,
-  USER: process.env.DB_USER,
-  PASSWORD: process.env.DB_PASSWORD,
-  DB: process.env.DB_DATABASE,
-  PORT: process.env.DB_PORT,
+  HOST: 'ec2-54-172-173-58.compute-1.amazonaws.com',
+  USER: 'zswzzvbafihehh',
+  PASSWORD: '1f9edd32e8ebbc872b5398eda2f2808040c7477d97e95052e6461947fbb5bd54',
+  DB: 'd3m3reom5eodi8',
+  PORT: 5432,
   dialect: 'postgresql',
   pool: {
     max: 5,
