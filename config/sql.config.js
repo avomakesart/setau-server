87de54d480fd677b72cbbe891d229bfa83bf18f6
Alvaro Castillo <alvaro.castillo777@gmail.com>
2020-09-08 02:27:17 -0500

    Adding configs for mysql on prod

--- config/sql.config.js
+++ config/sql.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   database: {
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'alvarocastle',
-    password: 'BLUEcat#2518',
-    database: 'setau',
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'b89c22dbd1a04f',
+    password: 'cfd8671d',
+    database: 'heroku_38e43dd30660a24',
   },
 }
