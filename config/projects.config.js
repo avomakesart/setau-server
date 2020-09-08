918fb247881399d99b25191223b584b99ab7a97a
Alvaro Castillo <alvaro.castillo777@gmail.com>
2020-09-08 02:58:29 -0500

    Updating db values postgres prod

--- config/projects.config.js
+++ config/projects.config.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-  user: 'alvarocastillocorrea',
-  password: 'Cosmosblue2508',
-  host: 'localhost',
+  user: 'zswzzvbafihehh',
+  password: '1f9edd32e8ebbc872b5398eda2f2808040c7477d97e95052e6461947fbb5bd54',
+  host: 'ec2-54-172-173-58.compute-1.amazonaws.com',
   port: 5432,
-  database: 'setau',
+  database: 'd3m3reom5eodi8',
 })
 
 module.exports = pool
